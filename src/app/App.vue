7140882c9f2af0e6f7fe1f9be4a2a180c3f04232
Learde <leardeleo@gmail.com>
2023-05-23 23:40:18 +0300

    Configure naive ui via config provider component

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,13 +1,18 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import { TheHeader } from "@/modules";
+import { NConfigProvider } from "naive-ui";
+
+import { naiveConfig } from "./naiveConfig";
 </script>
 
 <template>
+  <NConfigProvider :theme-overrides="naiveConfig">
     <TheHeader />
     <main>
         <RouterView />
     </main>
+  </NConfigProvider>
 </template>
 
 <style scoped>
---
+++ src/app/naiveConfig.js
@@ -0,0 +1,13 @@
+/**
+ * @type import('naive-ui').GlobalThemeOverrides
+ */
+export const naiveConfig = {
+    common: {
+        primaryColor: '#ff4b5f',
+        primaryColorHover: '#ff3148',
+        primaryColorPressed: '#ff233c',
+    },
+    Button: {
+        borderRadiusMedium: "6px",
+    }
+}
