7bef8995834da4011a9d6fb5bc75c4e681c846f5
Learde <leardeleo@gmail.com>
2023-05-23 22:09:29 +0300

    Import icons from figma design

---
+++ src/shared/icons/fluentEmoji.js
@@ -0,0 +1,10 @@
+// Icon search: https://icones.js.org/collection/fluent-emoji
+
+export {
+    IconBriefcase,
+    IconBarChart,
+    IconClipboard,
+    IconLaptop,
+    IconBooks,
+    IconBookmarkTabs
+} from "@iconify-prerendered/vue-fluent-emoji";
