4b623aeb3a18ea76d68bade0287b0cb6bbb4ad76
Learde <leardeleo@gmail.com>
2023-05-23 22:33:51 +0300

    Add logotip component to components layer and export it in the public api

---
+++ src/components/logo/BaseLogo.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+<div :class="classes.logoWrapper">
+  <img src="/src/shared/images/scb_logo_red_black.svg" alt="Совкомбанк">
+  <span>CareerConnect</span>
+</div>
+</template>
+
+<style module="classes">
+.logoWrapper {
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+}
+
+.logoWrapper img {
+  width: 240px;
+  height: auto;
+}
+
+.logoWrapper span {
+  font-size: 16px;
+}
+</style>
