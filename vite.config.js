3fe7440c4ec58198b8a57973b05732ac9981cfaa
Learde <leardeleo@gmail.com>
2023-05-24 03:56:43 +0300

    Install and configure eslint, stylelint and prettier

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
-import { fileURLToPath, URL } from 'node:url'
+/* eslint-disable node/no-unpublished-import */
+import { URL, fileURLToPath } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
+import stylelint from "vite-plugin-stylelint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [
+        vue(),
+        eslint({
+            include: "**/*.{js,ts,vue}",
+            exclude: ["**/node_modules/**"],
+        }),
+        stylelint({
+            dev: true,
+            fix: true,
+            include: "**/*.{css,scss,sass,vue}",
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+});
