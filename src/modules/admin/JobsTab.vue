988497accd387a1187cc7f933ff793d5713651a2
Learde <leardeleo@gmail.com>
2023-05-26 23:25:57 +0300

    Add button to the jobs tab

--- src/modules/admin/JobsTab.vue
+++ src/modules/admin/JobsTab.vue
@@ -1,10 +1,33 @@
 <script setup>
-import Card from "./JobCard.vue";
+import JobCard from "./JobCard.vue";
 </script>
 
 <template>
-    Вакансии
-    <Card />
+    <div :class="classes.menu">
+        <NButton type="primary">Создать вакансию</NButton>
+    </div>
+    <div :class="classes.list">
+        <JobCard />
+        <JobCard />
+        <JobCard />
+        <JobCard />
+    </div>
 </template>
 
-<style scoped></style>
+<style module="classes">
+.menu {
+    display: flex;
+
+    justify-content: center;
+
+    margin: 20px 0 30px;
+}
+
+.list {
+    display: grid;
+
+    grid-template-columns: 1fr;
+
+    gap: 20px;
+}
+</style>
