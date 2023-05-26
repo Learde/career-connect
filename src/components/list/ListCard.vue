b4f3762414160d778d7e07f9f7936b8555421ffa
Learde <leardeleo@gmail.com>
2023-05-26 23:04:10 +0300

    Add list card component and new color variable

---
+++ src/components/list/ListCard.vue
@@ -0,0 +1,87 @@
+<script setup>
+import { IconClose, IconDelete, IconEdit } from "@/shared";
+
+defineProps({
+    hasEdit: { type: Boolean, default: false },
+    hasClose: { type: Boolean, default: false },
+    hasDelete: { type: Boolean, default: false },
+});
+</script>
+
+<template>
+    <div :class="classes.card">
+        <div :class="classes.header">
+            <h4><slot name="title" /></h4>
+            <div :class="classes.icons">
+                <IconEdit
+                    v-if="hasEdit"
+                    :class="classes.icon"
+                    @click="$emit('edit')"
+                />
+                <IconClose
+                    v-if="hasClose"
+                    :class="classes.icon"
+                    @click="$emit('close')"
+                />
+                <IconDelete
+                    v-if="hasDelete"
+                    :class="classes.icon"
+                    @click="$emit('delete')"
+                />
+            </div>
+        </div>
+        <div :class="classes.content">
+            <slot name="content" />
+        </div>
+        <div :class="classes.footer">
+            <slot name="footer" />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" module="classes">
+.card {
+    width: 100%;
+    min-height: 105px;
+    padding: 12px 35px;
+
+    background: #ffecef;
+    border-radius: 8px;
+}
+
+.header {
+    display: flex;
+
+    justify-content: space-between;
+
+    margin-bottom: 5px;
+
+    font-size: 15px;
+
+    font-weight: 500;
+}
+
+.icons {
+    display: flex;
+
+    gap: 15px;
+}
+
+.icon {
+    color: #7a7a7a;
+
+    cursor: pointer;
+
+    transition: all 0.2s;
+
+    &:hover {
+        color: var(--c-secondary);
+    }
+}
+
+.content {
+    margin-bottom: 10px;
+
+    font-size: 15px;
+}
+</style>
